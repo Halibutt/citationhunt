226d79ba95b6773178d4a2d3eff82f71e02838b5
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2016-02-13 23:38:35 +0000

    Can't use a function on tools labs.

--- handlers/stats.py
+++ handlers/stats.py
@@ -8,19 +8,14 @@ import os
 import json
 import re
 
-def load_is_crawler(stats_db):
+def load_is_not_crawler(stats_db):
     crawler_user_agents = json.load(
         file(os.path.join(
             os.path.dirname(__file__),
             'crawler-user-agents', 'crawler-user-agents.json')))
-    with stats_db as cursor, chdb.ignore_warnings():
-        cursor.execute('DROP FUNCTION IF EXISTS IS_CRAWLER')
-        cursor.execute('''
-            CREATE FUNCTION IS_CRAWLER (user_agent VARCHAR(1024))
-            RETURNS BOOLEAN DETERMINISTIC RETURN (''' +
-            ' OR '.join(
-                'user_agent REGEXP "%s"' % obj['pattern']
-                for obj in crawler_user_agents) + ')')
+    return ' AND '.join(
+        'user_agent NOT REGEXP "%s"' % obj['pattern']
+        for obj in crawler_user_agents)
 
 def stats():
     days = flask.request.args.get('days', 14)
@@ -47,7 +42,7 @@ def stats():
 
     graphs = [] # title, data table as array, type
     stats_db = get_stats_db()
-    load_is_crawler(stats_db)
+    is_not_crawler = load_is_not_crawler(stats_db)
     stats_cursor = stats_db.cursor()
     lang_cursors = {lc: get_db(lc).cursor() for lc in lang_codes}
 
@@ -55,8 +50,8 @@ def stats():
         SELECT DATE_FORMAT(ts, GET_FORMAT(DATE, 'ISO')) AS dt,
         COUNT(*), lang_code FROM requests
         WHERE snippet_id IS NOT NULL AND status_code = 200
-        AND DATEDIFF(NOW(), ts) <= %s AND IS_CRAWLER(user_agent) = 0
-        GROUP BY dt, lang_code ORDER BY dt, lang_code''', (days,))
+        AND DATEDIFF(NOW(), ts) <= %s AND ''' + is_not_crawler +
+        '''GROUP BY dt, lang_code ORDER BY dt, lang_code''', (days,))
     graphs.append((
         'Number of snippets served in the past %s days' % days,
         json.dumps(rows_to_data_table('Date', list(stats_cursor))), 'line'))
@@ -68,8 +63,8 @@ def stats():
             WHERE snippet_id IS NOT NULL AND category_id IS NOT NULL AND
             category_id != "all" AND status_code = 200
             AND DATEDIFF(NOW(), ts) <= %s AND lang_code = %s
-            AND IS_CRAWLER(user_agent) = 0
-            GROUP BY category_id ORDER BY COUNT(*) DESC LIMIT 30
+            AND ''' + is_not_crawler +
+            '''GROUP BY category_id ORDER BY COUNT(*) DESC LIMIT 30
         ''', (days, lc))
         for category_id, count, user_agent in stats_cursor:
             c = lang_cursors[lc]
